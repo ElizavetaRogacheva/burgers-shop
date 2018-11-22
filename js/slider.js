f418ae66f83af9f77fb1316f0c9b6774a7fa9e5d
Елизавета Рогачева <liza-97-97-97@mail.ru>
2018-11-22 21:52:55 +0500

    закрытие состава при нажатие на крестик

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,22 @@
 var compositionBtn = document.querySelector('.composition');
 var compositionList = document.querySelector('.composition__details');
+var close = document.querySelector('.close-details');
+
 
 var compositionBtnClickHandler = function () {
     compositionList.classList.toggle('hidden');
 }
 
+var closeClickHandler = function () {
+    compositionList.classList.add('hidden');
+}
+
 compositionBtn.addEventListener('click', function (evt) {
     evt.preventDefault();
     compositionBtnClickHandler();
+})
+
+close.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    closeClickHandler();
 })
