418167173670d2f70925df8c41e810bbc4fca56b
Елизавета Рогачева <liza-97-97-97@mail.ru>
2018-11-29 00:51:43 +0500

    подключение fullpage scroll

--- js/slider.js
+++ js/slider.js
@@ -25,3 +25,6 @@ $('.slider__list').slick({
     prevArrow: '<a href="#" class="slide__arrow slide__arrow--prev"><svg class="slide__arrow-img" width = "27px" height="13px" fill = "#fff"><use xlink:href = "img/svg/sprite.svg#arrow-scroll"></use></svg></a>',
 	nextArrow: '<a href="#" class="slide__arrow slide__arrow--next"><svg class="slide__arrow-img" width = "27px" height="13px" fill = "#fff"><use xlink:href = "img/svg/sprite.svg#arrow-scroll"></use></svg></a>',
 });
+
+
+$('#fullpage').fullpage({});
